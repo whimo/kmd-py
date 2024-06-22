Fix off-by-one error in node_calculator function.

kmd-py/KMD.py
@@ -63,7 +63,7 @@ def KMD(X, Y, M=None, Knn=1, Kernel="discrete"):
         U_stats = (n_i.T @ Kernel @ n_i - np.sum(np.diag(Kernel) * n_i)) / n / (n - 1)
     
     def node_calculator(j):
-        return np.sum(Kernel[Y[j] - 1, Y[nn_index_X[j, :] - 1]])
+        return np.sum(Kernel[Y[j] - 1, Y[nn_index_X[j, :] - 1] - 1])
 
     if discrete_kernel:
         mean_Kii = 1