834dbc10291a30c7fbfe58510c24c93bae696db3
whimo <whimox@gmail.com>
2024-06-22 13:45:32 +0400

    Refactored KMD_MST function to improve readability and maintainability.

--- kmd-py/KMD_MST.py
+++ kmd-py/KMD_MST.py
@@ -2,6 +2,7 @@ import numpy as np
 import igraph
 import mlpack
 
+
 def KMD_MST(X, Y, M, discrete_kernel, Kernel, n_i):
     n = X.shape[0]
     if discrete_kernel:
@@ -14,10 +15,15 @@ def KMD_MST(X, Y, M, discrete_kernel, Kernel, n_i):
 
     if X.shape[1] == 1:
         Y = Y[np.argsort(X[:, 0])]
+
         def node_calculator(j):
             return Kernel[Y[j] - 1, Y[j - 1] - 1] + Kernel[Y[j] - 1, Y[j + 1] - 1]
+
         res = Kernel[Y[0] - 1, Y[1] - 1] + Kernel[Y[n - 1] - 1, Y[n - 2] - 1]
-        return ((np.sum([node_calculator(j) for j in range(1, n - 1)]) / 2 + res) / n - U_stats) / (mean_Kii - U_stats)
+        return (
+            (np.sum([node_calculator(j) for j in range(1, n - 1)]) / 2 + res) / n
+            - U_stats
+        ) / (mean_Kii - U_stats)
 
     if X.shape[0] == X.shape[1]:
         graph = igraph.Graph.Weighted_Adjacency(X.tolist(), mode=igraph.ADJ_UNDIRECTED)
@@ -25,7 +31,7 @@ def KMD_MST(X, Y, M, discrete_kernel, Kernel, n_i):
         edges = np.array(mst.get_edgelist())
     else:
         result = mlpack.emst(input=X)
-        edges = result['output'][:, :2].astype(int)
+        edges = result["output"][:, :2].astype(int)
 
     tmp = np.zeros((n, 2))
     for i in range(edges.shape[0]):
